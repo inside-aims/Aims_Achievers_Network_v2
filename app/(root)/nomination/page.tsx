63d07205efcd8e099a3d06462aa25b1ae079a9ee
Emmanuel <emmsom506@gmail.com>
2025-12-20 09:28:02 +0000

    Set up nomation page

---
+++ app/(root)/nomination/page.tsx
@@ -0,0 +1,11 @@
+
+import NominationProcess from "@/components/features/nomiation/nomination-process";
+
+const NominationPage = () => {
+  return(
+    <div id={"nomination"} className={"feature"}>
+      <NominationProcess/>
+    </div>
+  )
+}
+export default NominationPage
---
+++ components/features/nomiation/nomination-process.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+const NominationProcess = () => {
+  return(
+    <div>
+      NominationProcess
+    </div>
+  )
+}
+export default NominationProcess
