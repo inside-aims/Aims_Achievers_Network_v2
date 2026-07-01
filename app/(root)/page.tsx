5dfd146d153ad97532bb9afe85a48521c7017172
Emmanuel <emmsom506@gmail.com>
2026-07-01 11:22:34 +0000

    Chore home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,28 @@
+import ClosingCta from "@/components/features/home/ClosingCta";
+import EventPaths from "@/components/features/home/EventPaths";
 import FeatureGridBento from "@/components/features/home/FeatureGrid";
+import Hero from "@/components/features/home/Hero";
 import OurEvents from "@/components/features/home/OurEvents";
-import StackingCardsHero from "@/components/features/home/StackingCards";
+import Pillars from "@/components/features/home/Pillars";
+import ProductShowcase from "@/components/features/home/ProductShowcase";
+import Testimonials from "@/components/features/home/Testimonials";
 import TrustedPartners from "@/components/features/home/TrustedPartners";
 import ValuePropositionSection from "@/components/features/home/ValuePropositionSection";
 
 export default function Home() {
   return (
     <main>
-      <StackingCardsHero />
-      <div className="relative z-10 pt-20">
+      <Hero />
+      <div className="relative z-10">
+        <Pillars />
+        <EventPaths />
+        <ProductShowcase />
         <ValuePropositionSection />
-        {/*<ScrollBasedVelocityDemo />*/}
         <FeatureGridBento />
         <OurEvents />
+        <Testimonials />
         <TrustedPartners />
+        <ClosingCta />
       </div>
     </main>
   );
